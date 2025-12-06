4ef4b19e872d9cd0737c4cb7f8db4c0f6d850cc8
sebenemaryamashebir-cmd <sebenemaryamashebir@gmail.com>
2025-12-06 18:24:57 +0000

    Implement Meal class

--- Meal.java
+++ Meal.java
@@ -1,1 +1,70 @@
+public class Meal implements MealInterface {
+    private String name;
+    private FoodItem[] foodItems;
 
+    // Constructor
+    public Meal(String name, FoodItem[] foodItems) {
+        this.name = name;
+        this.foodItems = foodItems;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public FoodItem[] getFoodItems() {
+        return foodItems;
+    }
+
+    // ---------- Calculations ----------
+
+    @Override
+    public double calculateTotalCarbonFootprints() {
+        double total = 0;
+        for (FoodItem item: foodItems) {
+            total += item.getCarbonFootprint();
+        }
+        return total;
+    }
+
+    @Override
+    public double calculateTotalNitrogenFootprints() {
+        double total = 0;
+        for (FoodItem item: foodItems) {
+            total += item.getNitrogenFootprint();
+        }
+        return total;
+    }
+
+    @Override
+    public double calculateTotalFootprints() {
+        return calculateTotalCarbonFootprints() + calculateTotalNitrogenFootprints();
+    }
+
+    @Override
+    public double calculateTotalWaterUsage() {
+        double total = 0;
+        for (FoodItem item: foodItems) {
+            total += item.getWaterUsage();
+        }
+        return total;
+    }
+
+    @Override
+    public double calculateLandUsage() {
+        double total = 0;
+        for (FoodItem item: foodItems) {
+            total += item.getLandUsage();
+        }
+        return total;
+    }
+
+    @Override
+    public double calculateNitrogenWaste() {
+        double total = 0;
+        for (FoodItem item: foodItems) {
+            total += item.getNitrogenWaste();
+        }
+        return total;
+    }
+}
